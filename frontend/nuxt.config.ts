b90d094ffcdc78b189827baaf7c0e2c27977d850
Karan-Taneja <karan.tk.taneja@gmail.com>
2024-09-13 05:26:58 -0400

    chore: dockerize project

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   apollo: {
     clients: {
       default: {
-        httpEndpoint: 'http://localhost:8000/graphql' 
+        httpEndpoint: 'http://localhost:8080/graphql' 
       }
     }
   },
