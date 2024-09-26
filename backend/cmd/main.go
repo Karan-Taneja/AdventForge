2518ec690cbf3356cf7b22405c1355bdf27039da
TK <karan.tk.taneja@gmail.com>
2024-09-26 19:32:47 -0400

    refactor(backend): use router in main

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Karan-Taneja/AdventForge/internal/router"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	// TODO: Add router
+	// Load environment variables from .env file
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	// Initialize the router
+	r := router.NewRouter()
+
+	// Start the server
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("Starting server on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
